bbb78d47462a24f3bcfc2bab0ac080370262ea49
Arnaud Bailly <arnaud.oqube@gmail.com>
2016-06-13 17:42:15 +0200

    replace MVar with Chan

--- src/System/ProcessControl.hs
+++ src/System/ProcessControl.hs
@@ -2,6 +2,7 @@
 module System.ProcessControl where
 
 import           Control.Concurrent
+import           Control.Concurrent.Chan
 import           Control.Exception
 import           Control.Monad
 import           Data.Either
@@ -10,7 +11,7 @@ import           Data.Monoid
 import           System.Console.GetOpt
 import           System.Environment
 import           System.IO
-import           System.Process        as P
+import           System.Process          as P
 
 type ProcessControl = (Maybe Handle, Maybe Handle, Maybe Handle, ProcessHandle)
 
@@ -30,26 +31,32 @@ process TrackedProcess{..} = createProcess
 
 data Tracker = Tracker { trackerName :: String, trackedProcess :: ProcessHandle, trackerThreads ::  [ThreadId]  }
 
+instance Show Tracker where
+  show Tracker{..} = "Tracker { trackerName = " ++ trackerName ++ ", trackerThreads = "++ show trackerThreads ++ "}"
+
 data Message = Message { origin :: String, msgColor :: String, msg :: String }
 
-trackProcess :: MVar Message -> TrackedProcess  -> IO (Either IOException Tracker)
+trackProcess :: Chan Message -> TrackedProcess  -> IO (Either IOException Tracker)
 trackProcess chan t@TrackedProcess{..} = do
   procStart <- try (process t)
-  let readOutput hd = forkIO $ forever $ do
-       l <- hGetLine hd
-       putMVar chan (Message name color l)
   case procStart of
    Right (Nothing, Just aout, Just aerr, aProc) -> do
      outtid <- readOutput aout
      errtid <- readOutput aerr
-     putStrLn $ "\x1b[31mstarted " ++ execPath ++ " " ++ concat (intersperse " " args) ++ "\x1b[0m"
-     return $ Right $ Tracker name aProc [outtid, errtid]
+     putStrLn $ "\x1b[31mstarted " ++ name ++ " (" ++ execPath ++ " " ++ concat (intersperse " " args) ++ ")\x1b[0m"
+     let tr = Tracker name aProc [outtid, errtid]
+     return $ Right tr
    Left e -> print e >> (return $ Left e)
+
+   where
+     readOutput hd = forkIO $ forever $ do
+       l <- hGetLine hd
+       writeChan chan (Message name color l)
 
 trackProcesses :: [ TrackedProcess ] -> IO ()
 trackProcesses toTrack = do
-  chan <- newEmptyMVar
+  chan <- newChan
   procs <- mapM (trackProcess chan) toTrack
   when (null (lefts procs)) $ forever $ do
-             out <- takeMVar chan
-             putStrLn $ msgColor out <> "[" <> origin out <> "] - \x1b[0m" <> msg out
+    out <- readChan chan
+    putStrLn $ msgColor out <> "[" <> origin out <> "] - \x1b[0m" <> msg out
